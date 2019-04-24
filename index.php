17dc956d894ec820dc199f00f89ba5c82a456082
johanodell <35562269+johanodell@users.noreply.github.com>
2019-04-24 14:35:58 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<head>
+<title>
+TEST
+</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+</head>
+<html> 
+<body style="background-color: white">
+<p>
+<h1>
+Version 3
+<br>
+<?php 
+  echo "This pod IP is: ".$_SERVER['SERVER_ADDR'] ;
+  echo "\n<br>" ;
+  date_default_timezone_set('Europe/Stockholm');
+  $date = date('Y/m/d H:i:s', time());
+  echo "\nThe Server time is: " .$date ; ?>
+</h1>
+</p>
+</body>
+</html>
